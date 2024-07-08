1730772df67f1c796e07a6d8453f21e0741ec384
cedgestioneambiente <72556161+cedgestioneambiente@users.noreply.github.com>
2024-07-08 09:00:27 +0200

    20240708 - added & updated migrations scripts

---
+++ src/GaCloudServer.Admin.EntityFramework.Shared/MigrationHelpers/get-migrations.ps1
@@ -0,0 +1,25 @@
+﻿$projectName = "GaCloudServer";
+$currentPath = Get-Location
+Set-Location "../../$projectName.Admin"
+
+
+
+#Initialze db context and define the target directory
+$targetContexts = @{ 
+    dbContext= "Migrations\Resources"
+}
+
+#Initialize the db providers and it's respective projects
+$dpProviders = @{
+    SqlServer  = "..\..\src\$projectName.Admin.EntityFramework.SqlServer\$projectName.Admin.EntityFramework.SqlServer.csproj";
+    PostgreSQL = "..\..\src\$projectName.Admin.EntityFramework.PostgreSQL\$projectName.Admin.EntityFramework.PostgreSQL.csproj";
+    MySql      = "..\..\src\$projectName.Admin.EntityFramework.MySql\$projectName.Admin.EntityFramework.MySql.csproj";
+}
+
+$projectPath=(Get-Item -Path $dpProviders['SqlServer'] -Verbose).FullName;
+$context='ResourcesDbContext';
+
+dotnet ef migrations list -c $context -p $projectPath
+
+Set-Location $currentPath
+
---
+++ src/GaCloudServer.Admin.EntityFramework.Shared/MigrationHelpers/last-migrations.ps1
@@ -0,0 +1,32 @@
+﻿$projectName = "GaCloudServer"
+$currentPath = Get-Location
+Set-Location "../../$projectName.Admin.EntityFramework.SqlServer"
+
+# Directory delle migrazioni
+$migrationsDir = "Migrations\Resources"
+
+# Variabile opzionale per filtro
+$filterString = $args[0]
+
+# Ottieni tutti i file di migrazione, escludendo il ModelSnapshot
+$migrationFiles = Get-ChildItem $migrationsDir -Filter "*.cs" | 
+                  Where-Object { $_.Name -notmatch "ModelSnapshot" }
+
+# Applica il filtro se $filterString è valorizzato
+if ($filterString) {
+    $migrationFiles = $migrationFiles | Where-Object { $_.Name -like "*$filterString*" }
+}
+
+# Ordina i file per data di creazione (dal più recente al più vecchio)
+$migrationFiles = $migrationFiles | Sort-Object LastWriteTime -Descending
+
+# Verifica se ci sono abbastanza file per ottenere il penultimo
+if ($migrationFiles.Count -ge 1) {
+    # Il penultimo file se esiste
+    $latestMigration = $migrationFiles[0]
+    Write-Output "Ultima migrazione: $($latestMigration.Name)"
+} else {
+    Write-Output "Nessuna migrazione trovata."
+}
+
+Set-Location $currentPath
